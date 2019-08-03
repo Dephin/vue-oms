185b64eda512c05109820bc129bc44ac051692a7
Huanglin <huanglin@nengmao.net>
2019-08-03 18:29:35 +0800

    install dependency

--- vue-oms/src/main.js
+++ vue-oms/src/main.js
@@ -1,10 +1,18 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
+import Vuex from 'vuex'
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
 import App from './App'
 import router from './router'
+import axios from 'axios'
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
+
+Vue.use(Vuex);
+Vue.use(ElementUI);
+Vue.prototype.$axios = axios;
 
 /* eslint-disable no-new */
 new Vue({
@@ -12,4 +20,4 @@ new Vue({
   router,
   components: { App },
   template: '<App/>'
-})
+});
